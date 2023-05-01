2f057b1a9f6d5a822b77785f5f897888010f1430
iggosha <iegolovkov@gmail.com>
2023-05-01 14:10:46 +0300

    Небольшие изменения шрифтов и цвета страниц

--- js/makeOrder.js
+++ js/makeOrder.js
@@ -15,9 +15,13 @@ function openOrderWindow() {
   let win = window.open("", "", "width=720,height=480");
   win.document.open();
   win.document.write('<head> <title>Заказ</title> <link rel="stylesheet" href="css/mystyle.css"/> </head>')
-  win.document.write("Ваш заказ:<br><br>");
-  win.document.write(text);
-  win.document.write('<input type="button" value="Подтвердить" onClick="window.close();" style="width: 300px; height: 100px">');
+  win.document.write("<h1>Ваш заказ:</h1>");
+  if (text!="") {
+    win.document.write(text);
+  } else {
+    win.document.write("Ничего не выбрано<br>");
+  }
+  win.document.write('<input type="button" value="Подтвердить и закрыть" onClick="window.close();" style="width: 500px; height: 75px">');
   win.document.write('<h6 style="text-align: right; font-size: 0.5em">При ошибке перезагрузите главную страницу</h6>')
   win.document.close();
 
