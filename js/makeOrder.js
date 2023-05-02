d15ea6cbd84a05b46952c9460eb20cc497a6fce8
iggosha <iegolovkov@gmail.com>
2023-05-02 04:13:41 +0300

    Добавлена заготовка под иконку корзины, небольшие косметические изменения

--- js/makeOrder.js
+++ js/makeOrder.js
@@ -14,16 +14,15 @@ function addProducts() {
 function openOrderWindow() {
   let win = window.open("", "", "width=720,height=480");
   win.document.open();
-  win.document.write('<head> <title>Заказ</title> <link rel="stylesheet" href="css/mystyle.css"/> </head>')
+  win.document.write('<head> <title>Заказ</title> <link rel="stylesheet" href="css/mystyle.css"/> <script src="js/makeOrder.js"></script> </head>');
   win.document.write("<h1>Ваш заказ:</h1>");
-  if (text!="") {
+  if (text != "") {
     win.document.write(text);
   } else {
     win.document.write("Ничего не выбрано<br>");
   }
   win.document.write('<input type="button" value="Подтвердить и закрыть" onClick="window.close();" style="width: 500px; height: 75px">');
-  win.document.write('<h6 style="text-align: right; font-size: 0.5em">При ошибке перезагрузите главную страницу</h6>')
+  win.document.write('<h6 style="text-align: right; font-size: 0.5em">При ошибке перезагрузите главную страницу</h6>');
   win.document.close();
-
-
+  text = "";
 }
